1cec08755d3b3b452aa89e9293e118eb85bcbfda
Mina Aboalim <mina_aboalim@hotmail.com>
2020-10-07 14:36:43 +0200

    init code for DB guide project

---
+++ README.md
@@ -0,0 +1,10 @@
+This is Test Project using Spring boot.
+
+## PREREQUISITE
+1.  Operating system Windows or Centos.
+2.  Java 1.8 or higher.
+3.  IDE eclipse
+4.  Maven
+
+## TESTING
+Testing the application using Junit
---
+++ src/test/java/com/aggroup/dbguide/DbGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggroup.dbguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
